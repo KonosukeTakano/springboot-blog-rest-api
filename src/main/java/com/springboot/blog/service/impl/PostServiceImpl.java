6f458624006c8bcf8182832ac3345bb72e3f7eec
nosukebe <bado.bado.0408@gmail.com>
2022-09-29 10:19:33 +0900

    [Upd] PostServiceImpl

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -14,14 +14,18 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+import org.modelmapper.ModelMapper;
 @Service
 public class PostServiceImpl implements PostService {
 
     private PostRepository postRepository;
 
-    public PostServiceImpl(PostRepository postRepository) {
+    private ModelMapper mapper;
+
+    public PostServiceImpl(PostRepository postRepositorym, ModelMapper mapper)
+    {
         this.postRepository = postRepository;
+        this.mapper = mapper;
     }
 
     @Override
@@ -93,20 +97,26 @@ public class PostServiceImpl implements PostService {
 
     // convert Entity into DTO
     private PostDto mapToDTO(Post post){
-        PostDto postDto = new PostDto();
-        postDto.setId(post.getId());
-        postDto.setTitle(post.getTitle());
-        postDto.setDescription(post.getDescription());
-        postDto.setContent(post.getContent());
+
+        PostDto postDto = mapper.map(post, PostDto.class);
+
+//        PostDto postDto = new PostDto();
+//        postDto.setId(post.getId());
+//        postDto.setTitle(post.getTitle());
+//        postDto.setDescription(post.getDescription());
+//        postDto.setContent(post.getContent());
         return postDto;
     }
 
     // convert DTO to entity
     private Post mapToEntity(PostDto postDto){
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+
+        Post post = mapper.map(postDto, Post.class);
+
+//        Post post = new Post();
+//        post.setTitle(postDto.getTitle());
+//        post.setDescription(postDto.getDescription());
+//        post.setContent(postDto.getContent());
         return post;
     }
 
