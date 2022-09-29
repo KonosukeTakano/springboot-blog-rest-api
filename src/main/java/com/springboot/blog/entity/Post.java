bf1c33b06d502691332a60169fcebcb72c62b49f
nosukebe <bado.bado.0408@gmail.com>
2022-09-30 01:10:00 +0900

    [Upd] Post.java

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -33,5 +33,5 @@ public class Post {
     private String content;
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Comment> comment = new HashSet<>();
+    private Set<Comment> comments = new HashSet<>();
 }
