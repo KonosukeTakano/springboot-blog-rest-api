71d461e383a6b9c8707c4f7be9f7cf8f50647e95
nosukebe <bado.bado.0408@gmail.com>
2022-10-07 02:10:16 +0900

    [Complete] REST API Validation

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,10 +2,22 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "Name shold not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email(message = "must be a well-formed email address")
     private String email;
+
+    @NotEmpty
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
     private String body;
 }
