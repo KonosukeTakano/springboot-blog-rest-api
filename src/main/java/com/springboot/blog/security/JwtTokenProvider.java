53e3368a0157c22f8d3872c1820b95120fa3680b
nosukebe <bado.bado.0408@gmail.com>
2022-10-14 08:18:21 +0900

    [Create] JwtTokenProvider

---
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -0,0 +1,34 @@
+package com.springboot.blog.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration-milliseconds}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationInMs);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm. http, jwtSecret)
+                .compaxt();
+                return token;
+    }
+
+    public String getUsername 移動From
+}
